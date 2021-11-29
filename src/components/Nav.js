b5bca27b9d522982c09a4a43c6f3e01713d0ff6b
Dawid2803 <dawidjacoba@gmail.com>
2021-11-29 21:11:37 +0200

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function Nav() {
     return (
         <div>
             <nav className="main-nav">
                 <ul>
-                    <Link to="#"> Cats </Link>
-                    <Link to="#"> Dogs </Link>
-                    <Link to="#"> Computers </Link>
+                    <NavLink to="#"> Cats </NavLink>
+                    <NavLink to="#"> Dogs </NavLink>
+                    <NavLink to="#"> Computers </NavLink>
                 </ul>
             </nav>
         </div>
